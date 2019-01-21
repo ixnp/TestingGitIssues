947c99fa94439d63672e4b030d38fbcae1c34670
Benjamin G. West <23172276+bgwest@users.noreply.github.com>
2019-01-21 14:46:35 -0800

    updated string checker

--- blablabla.js
+++ blablabla.js
@@ -1,6 +1,15 @@
 'use strict'
+
+# test if I am a string
 let testTest = function(str){
-  console.log(str)
+  let myStringObj = { string: true, data: str };
+  if (!str.length || !str.charAt(0)) {
+    myStringObj.string = false;
+    return myStringObj;
+  } // else 
+  return myStringObj;
 };
 
-testTest('test');
+let result = testTest('test');
+console.log(result);
+
